d663f06da127dac0959210c8d032c8c8aed00b5b
mvcrouse <mvcrouse@indiana.edu>
2015-09-02 21:18:00 -0500

    fixed minor bug

--- miniInterpreter.scm
+++ miniInterpreter.scm
@@ -4,17 +4,18 @@
   (lambda (expr)
     (eMatch expr
             ((+ ,x ,y) (+ x y))
-            ((and ,x ,y) (and x (interpret y)))
+            ((and ,x ,y ,z ***) (and (interpret x) (interpret (cons 'and (cons y z)))))
+            ((and ,x ,y) (and (interpret x) (interpret y)))
             (((lambda (,x) ,x) ,y) ((lambda (x) x) (interpret y)))
             (,x x))))
 
 
 ;;; Here's some test code
 
-(display (interpret ((lambda (z) z) 5)))
+(display (interpret '((lambda (z) z) 5)))
 (printf "\n")
-(display (interpret (and #t #t #t #f)))
+(display (interpret '(and #t #t #f #t #t #t)))
 (printf "\n")
-(display (interpret (and #t #t #t)))
+(display (interpret '(and #t 5)))
 (printf "\n")
-(display (interpret (+ 1 2)))
+(display (interpret '(+ 1 2)))
